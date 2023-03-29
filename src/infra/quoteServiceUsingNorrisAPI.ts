482e2659d1edb62fc5bcdcfb58e90172eee6e961
j.m.dejager <j.m.dejager@scienta.nl>
2023-03-29 20:55:59 +0200

    get quotes from norris api fixes #4

--- src/infra/quoteServiceUsingNorrisAPI.ts
+++ src/infra/quoteServiceUsingNorrisAPI.ts
@@ -1,10 +1,27 @@
-import {Injectable} from "@angular/core";
-import {Quote} from "src/domain/quote";
+import {Inject, Injectable} from "@angular/core";
+import {Quote} from "../domain/quote";
 import {QuoteServiceInterface} from "../domain/quoteService.interface";
+import {HttpClient} from "@angular/common/http";
+import {firstValueFrom, map} from "rxjs";
+
+interface NorrisApiContract {
+  categories: string[],
+  created_at: string,
+  icon_url: string,
+  id: string,
+  updated_at: string,
+  url: string,
+  value: string,
+}
 
 @Injectable()
 export class QuoteServiceUsingNorrisAPI implements QuoteServiceInterface {
-  public getQuote(): Quote | Promise<Quote> {
-    return Quote.fromRecord({author: ' ', text: ' '});
+
+  private httpClient: HttpClient = Inject(HttpClient);
+
+  public getQuote(): Promise<Quote> {
+    return firstValueFrom(this.httpClient.get<NorrisApiContract>('https://api.chucknorris.io/jokes/random').pipe(
+      map((res) => Quote.fromRecord({author: 'Chuck Norris', text: res.value}))
+    ));
   }
 }
